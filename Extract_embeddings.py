0b93f59d03d33f10afafe48168c4ae3246480d58
NotHacking (https://silverflag.net) <yetanotherdingus@gmail.com>
2024-11-17 14:27:24 +0100

    now my code is here

--- Extract_embeddings.py
+++ Extract_embeddings.py
@@ -1,9 +1,9 @@
+import os
+import cv2
 from imutils import paths
 import numpy as np
 import imutils
 import pickle
-import cv2
-import os
 
 BASE_DIR = os.path.dirname(__file__)
 print("[INFO] BASE DIR: ", BASE_DIR)
@@ -22,35 +22,41 @@ print("[DEBUG] Image Paths: ", imagePaths)
 knownEmbeddings = []
 knownNames = []
 total = 0
-for (i, imagePath) in enumerate(imagePaths):
-	name = imagePath.split(os.path.sep)[-2]
-	image = cv2.imread(imagePath)
-	image = imutils.resize(image, width=600)
-	(h, w) = image.shape[:2]
-	dnn.blobFromImage(
-		cv2.resize(image, (300, 300)), 1.0, (300, 300),
-		(104.0, 177.0, 123.0), swapRB=False, crop=False)
-	detector.setInput(imageBlob)
-	detections = detector.forward()
-	if len(detections) > 0:
-		i = np.argmax(detections[0, 0, :, 2])
-		confidence = detections[0, 0, i, 2]
-		print("[DEBUG] Confidence: ", confidence)
 
-		if confidence > 0.5:
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
-			face = image[startY:endY, startX:endX]
-			(fH, fW) = face.shape[:2]
-			if fW < 20 or fH < 20:
-				continue
-			faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
-				(96, 96), (0, 0, 0), swapRB=True, crop=False)
-			embedder.setInput(faceBlob)
-			vec = embedder.forward()
-			knownNames.append(name)
-			knownEmbeddings.append(vec.flatten())
-			total += 1
+for (i, imagePath) in enumerate(imagePaths):
+    name = imagePath.split(os.path.sep)[-2]
+    image = cv2.imread(imagePath)
+    if image is None:
+        print(f"[ERROR] Unable to load image at {imagePath}. Skipping...")
+        continue
+    image = imutils.resize(image, width=600)
+    (h, w) = image.shape[:2]
+    imageBlob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)), 1.0, (300, 300),
+        (104.0, 177.0, 123.0), swapRB=False, crop=False)
+    detector.setInput(imageBlob)
+    detections = detector.forward()
+
+    if len(detections) > 0:
+        i = np.argmax(detections[0, 0, :, 2])
+        confidence = detections[0, 0, i, 2]
+        print("[DEBUG] Confidence: ", confidence)
+
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            face = image[startY:endY, startX:endX]
+            (fH, fW) = face.shape[:2]
+            if fW < 20 or fH < 20:
+                continue
+            faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+                                             (96, 96), (0, 0, 0), swapRB=True, crop=False)
+            embedder.setInput(faceBlob)
+            vec = embedder.forward()
+            knownNames.append(name)
+            knownEmbeddings.append(vec.flatten())
+            total += 1
+
 # Dump to the disk.
 data = {"embeddings": knownEmbeddings, "names": knownNames}
 print("[DEBUG] Total Faces:", total)
